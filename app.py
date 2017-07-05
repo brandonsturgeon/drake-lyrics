c08761d3f67223f296c21d19b2cea9f0d1972f8b
Brandon Sturgeon <sturgeonb4@gmail.com>
2017-07-05 14:04:41 -0500

    Move lyrics to main folder

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pickle
 from random import choice
 
 app = Flask(__name__)
-lyrics_file = './build-lyrics/all_lyrics.txt'
+lyrics_file = './all_lyrics.txt'
 
 lyrics = ['yeah     ']
 with open(lyrics_file, 'rb') as lyric_file:
