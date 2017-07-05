e283ba281808277054232103ce49eff9282fa89f
Brandon Sturgeon <sturgeonb4@gmail.com>
2017-07-05 14:24:34 -0500

    Add static folder with favicon

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 import pickle
 from random import choice
 
@@ -16,3 +16,7 @@ def random_lyric():
 @app.route('/all_lyrics')
 def all_lyrics():
     return str(lyrics)
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
