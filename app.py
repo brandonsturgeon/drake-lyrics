0ec043ecc3da5b223040081e8a70b15f099a09fd
Brandon Sturgeon <sturgeonb4@gmail.com>
2017-07-06 09:41:28 -0500

    Add html template + api route

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory, render_template
 import os
 import pickle
 from random import choice
@@ -12,6 +12,12 @@ with open(lyrics_file, 'rb') as lyric_file:
 
 @app.route('/')
 def random_lyric():
+    chosen_lyric = choice(lyrics)
+    lyric_lines = chosen_lyric.split('\n')
+    return render_template('lyrics.html', lyric_lines=lyric_lines)+'\n'
+
+@app.route('/lyric')
+def api_lyric():
     return choice(lyrics)+'\n'
 
 @app.route('/all_lyrics')
